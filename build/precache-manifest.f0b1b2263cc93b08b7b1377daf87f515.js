7df7c17d5e9c9d708844f461d4206742f8bed8cd
cmattey <cmattey@dons.usfca.edu>
2020-01-03 08:40:15 -0800

    Added express static build, and build files from frontend app

---
+++ build/precache-manifest.f0b1b2263cc93b08b7b1377daf87f515.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cf06f41c30cbff30af6d75c3c2a1f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf814ee1bc437e20dfc3",
+    "url": "/static/js/2.0dc0cc14.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.0dc0cc14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9db948fc41c2271f5158",
+    "url": "/static/js/main.031f7fe7.chunk.js"
+  },
+  {
+    "revision": "f7fc8eba76c28cae35bb",
+    "url": "/static/js/runtime-main.a53a739b.js"
+  }
+]);
